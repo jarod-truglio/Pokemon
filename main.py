28090142901d5d8c11ebfc2a3f69abb6963d7997
jarod truglio <jarod.truglio@laplateforme.io>
2023-04-26 12:58:40 +0200

    Ajout combat V1

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import random 
 
 class Pokemon:
-    def __init__(self, nom, element, pa, niveau=1, defense=0, pv=100):
-        self.element = element
+    def __init__(self, nom, pa, niveau=1, defense=0, pv=100):
         self.nom = nom
         self.pv = pv
         self.niveau = niveau
@@ -16,42 +15,69 @@ class Pokemon:
 
 class Bulbizarre(Pokemon):
     def __init__(self, pa, niveau=1, defense=0, pv=100, nom="Bulbizarre", element="Plante"):
-        super().__init__(nom,  element, pa, niveau, defense, pv)
+        super().__init__(nom, pa, niveau, defense, pv)
+        self.element = element
         self.pa = 49
         self.defense = 49
-
 
     def Affiche_Bulbizarre(self):
         print("Sa Puissance d'attaque est de :", self.pa)
         print("Sa défense est de :", self.defense)
         print("Son niveau est de :", self.niveau)
         print("Ses Points de vie sont de :", self.pv)
+        print("Son élément est :", self.element)
+
+    def Attaque_Rapide(self):
+        if self.element == "Feu":
+            self.pv -= 24
+        if self.element == "Eau":
+            self.pv -= 98
+        if self.element == "Plante":
+            self.pv -= 49
 
 class Carapuce(Pokemon):
-    def __init__(self, pa, niveau=1, defense=0, pv=100, nom="Carapuce", element="Eau"):
-        super().__init__(nom, element,  pa, niveau, defense, pv)
+    def __init__(self, pa, niveau=1, defense=0, pv=100, nom="Carapuce",element="Eau"):
+        super().__init__(nom, pa, niveau, defense, pv)
         self.pa = 48
         self.defense = 65
-
+        self.element = element
 
     def Affiche_Carapuce(self):
         print("Sa Puissance d'attaque est de :", self.pa)
         print("Sa défense est de :", self.defense)
         print("Son niveau est de :", self.niveau)
         print("Ses Points de vie sont de :", self.pv)
+        print("Son élément est :", self.element)
+
+    def Attaque_Rapide(self):
+        if self.element == "Feu":
+            self.pv -= 96
+        if self.element == "Eau":
+            self.pv -= 48
+        if self.element == "Plante":
+            self.pv -= 24
 
 class Salameche(Pokemon):
-    def __init__(self,  pa,nom="Salameche", niveau=1, defense=0, pv=100, element="Feu"):
-        super().__init__(nom, element,  pa, niveau, defense, pv)
+    def __init__(self, pa,nom="Salameche", niveau=1, defense=0, pv=100, element="Feu"):
+        super().__init__(nom, pa, niveau, defense, pv)
         self.pa = 52
         self.defense = 43
-
+        self.element = element
 
     def Affiche_Salameche(self):
         print("Sa Puissance d'attaque est de :", self.pa)
         print("Sa défense est de :", self.defense)
         print("Son niveau est de :", self.niveau)
         print("Ses Points de vie sont de :", self.pv)
+        print("Son élément est :", self.element)
+
+    def Attaque_Rapide(self):
+        if self.element == "Feu":
+            self.pv -= 52
+        if self.element == "Eau":
+            self.pv -= 26
+        if self.element == "Plante":
+            self.pv -= 104
 
 p=Pokemon("",10)
 b = Bulbizarre("", 10)
@@ -67,4 +93,3 @@ elif p.nom == "Carapuce":
     c.Affiche_Carapuce()
 else:
     print("Erreur")
-
